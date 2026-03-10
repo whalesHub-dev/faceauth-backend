d25f0a1b0cfb201aab375b8fb53c520c8056df3a
whalesHub-dev <adewaleshedrack2020@gmail.com>
2026-03-10 09:02:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ async def ping():
     return {"status": "ok", "service": "FaceAuth API"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
